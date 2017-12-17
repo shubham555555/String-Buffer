9bb6fe90a7e5f7f0edad69b6dc09bbbf40c72fb9
shubham555555 <31390965+shubham555555@users.noreply.github.com>
2017-12-17 07:55:05 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.lang.String;
+public class Solution{
+  public static void main(String args[]){
+  StringBuffer s1="I am Shubham";
+  StringBuffer s2=new StringBuffer(" Waghade");
+  StringBuffer s3=new stringBuffer(10);
+  StringBuffer s4=s1.concat(s2);
+  System.out.println(s4);
+  }}
